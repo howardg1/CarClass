67652452a99fa1d2938ebf2841aa598dca47f99c
howardg <howardg@hartwick.edu>
2019-12-11 11:27:45 -0500

    car

--- carclass.py
+++ carclass.py
@@ -1,2 +1,11 @@
 # Gavin Howard 
 # Car Class
+
+
+class Car:
+    def __init__(self, make, model, year, engine):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.engine = engine
+        
